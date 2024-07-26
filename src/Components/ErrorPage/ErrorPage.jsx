9f1f134e51a63a217bb518a98988b57c3cc44817
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-07-26 23:34:39 +0600

    error page design completed

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,15 @@
-import { useRouteError } from "react-router-dom";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 
 const ErrorPage = () => {
 
     const error = useRouteError();
-
+    const navigate = useNavigate();
+    const handleBack = () => {
+        navigate(-1);
+    }
     return (
         <div className="flex flex-col justify-center items-center mt-32 space-y-4">
             <h1 className="text-3xl font-bold">Oops!</h1>
@@ -12,6 +17,9 @@ const ErrorPage = () => {
             <p className="font-bold">
                 {error.statusText || error.message}
             </p>
+            <div className="text-white">
+                <button onClick={handleBack} className="px-3 py-1  rounded bg-gradient-to-r from-primary-first to-primary-second"><span ><FontAwesomeIcon icon={faAngleLeft} /> Back</span></button>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-first': "#7e90fe",
+        'primary-second': '#9873ff'
+      }
+    },
+
   },
   plugins: [require('daisyui')],
 }
